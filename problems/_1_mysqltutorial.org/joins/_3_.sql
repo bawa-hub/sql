c3a9e90ee7a3b20f349125fc1ac0aaa11f345bae
Vikram Kumar <vikram.kuamr@squareboat.com>
2022-09-16 10:19:58 +0530

    joins problems added

---
+++ problems/_1_mysqltutorial.org/joins/_3_.sql
@@ -0,0 +1,18 @@
+-- uses two INNER JOIN clauses to join three tables: orders, orderdetails, and products:
+
+SELECT 
+    orderNumber,
+    orderDate,
+    orderLineNumber,
+    productName,
+    quantityOrdered,
+    priceEach
+FROM
+    orders
+INNER JOIN
+    orderdetails USING (orderNumber)
+INNER JOIN
+    products USING (productCode)
+ORDER BY 
+    orderNumber, 
+    orderLineNumber;
---
+++ problems/_1_mysqltutorial.org/joins/_8_.sql
@@ -0,0 +1,18 @@
+-- use two LEFT JOIN clauses to join the three tables: employees, customers, and payments
+
+SELECT 
+    lastName, 
+    firstName, 
+    customerName, 
+    checkNumber, 
+    amount
+FROM
+    employees
+LEFT JOIN customers ON 
+    employeeNumber = salesRepEmployeeNumber
+LEFT JOIN payments ON 
+    payments.customerNumber = customers.customerNumber
+ORDER BY 
+    customerName, 
+    checkNumber;
+
