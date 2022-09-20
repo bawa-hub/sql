370c783145ad6053cb46c58cac14ebaf7a313aae
bawa-hub <itsworkingmind@gmail.com>
2022-09-20 21:12:29 +0530

    problems added

--- _1_mysql/_22_administration/_4_backup_and_restore/restore_or_Execute.sh
+++ _1_mysql/_22_administration/_4_backup_and_restore/restore_or_Execute.sh
@@ -1,5 +1,5 @@
 mysql -u root -p
-
 use <db_name>;
-# excecute any file
+
+# restore or excecute any file
 source <sql_file>
---
+++ _1_mysql/_3_filtering_data/_2_select_distinct.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT
+    select_list
+FROM
+    table_name
+WHERE 
+    search_condition
+ORDER BY 
+    sort_expression;
