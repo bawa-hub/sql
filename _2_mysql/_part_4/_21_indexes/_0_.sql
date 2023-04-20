d00d8c168af8cd17004c1c2d90d2fd899636ee6f
Vikram kumar <vikram.kumar@squareboat.cm>
2023-04-20 12:26:34 +0530

    modifying data notes added

--- _2_mysql/_part_4/_21_indexes/_0_.sql
+++ _2_mysql/_part_4/_21_indexes/_0_.sql
@@ -1,9 +1,12 @@
--- MySQL uses indexes to quickly find rows with specific column values. Without an index, MySQL must scan the whole table to locate the relevant rows. 
+-- MySQL uses indexes to quickly find rows with specific column values. 
+-- Without an index, MySQL must scan the whole table to locate the relevant rows. 
 -- The larger table, the slower it searches.
 
 -- An index is a data structure such as B-Tree that improves the speed of data retrieval on a table 
 -- at the cost of additional writes and storage to maintain it.
 
 -- query optimizer may use indexes to quickly locate data without having to scan every row in a table for a given query.
--- When you create a table with a primary key or unique key, MySQL automatically creates a special index named PRIMARY. This index is called the clustered index.
+-- When you create a table with a primary key or unique key, 
+
+-- MySQL automatically creates a special index named PRIMARY. This index is called the clustered index.
 -- Other indexes other than the PRIMARY index are called secondary indexes or non-clustered indexes.
