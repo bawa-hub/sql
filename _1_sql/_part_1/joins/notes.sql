8918e813adaedfb7f2db2558fe647ad5b7bc36ab
Vikram kumar <vikram.kumar@squareboat.cm>
2023-04-30 10:47:50 +0530

    sql joins added

---
+++ _1_sql/_part_1/joins/notes.sql
@@ -0,0 +1,23 @@
+-- INNER JOIN
+-- Fetches matching records only
+
+-- LEFT JOIN
+-- INNER JOIN + additional records from left table
+
+-- RIGHT JOIN
+-- INNER JOIN + additional records from right table
+
+-- FULL OUTER JOIN/FULL JOIN
+-- INNER JOIN 
+--  + all remaining records from left table
+--  + all remaining records from right table
+
+-- CROSS JOIN/CARTESIAN JOIN
+-- returns cartesian product
+
+-- SELF JOIN
+-- inner join on same table
+
+-- NATURAL JOIN
+
+
