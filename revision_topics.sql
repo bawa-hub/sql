882cdf84f0473c18513f77f6a20cf8583a4c5498
Vikram kumar <vikram.kumar@squareboat.cm>
2023-04-30 12:31:56 +0530

    revision notes added

---
+++ revision_topics.sql
@@ -0,0 +1,41 @@
+-- operators
+-- data types 
+-- constraints
+
+-- db design
+-- normalization
+
+-- quering
+-- sorting
+-- filtering
+-- grouping
+-- joins
+-- subqueries
+-- sets operators
+-- cte
+
+-- managing databases
+-- managing tables
+-- modifying data
+-- transactions
+
+-- views
+-- indexes
+-- triggers
+-- stored procedures
+-- cursors
+
+-- built in functions
+-- full text search
+
+-- administration
+
+-- db internals
+-- CAP theorem
+-- ACID
+-- concurrency and locking
+-- sharding and partioning
+-- replication
+-- storage engines
+-- security
+-- system design
